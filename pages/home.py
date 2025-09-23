bf4615b1102a53521335bf47ebe7f0e8469ecfcd
DimasEka <76155589+masdimaseka@users.noreply.github.com>
2025-09-23 21:16:40 +0800

    fix: change home desc

--- pages/home.py
+++ pages/home.py
@@ -14,7 +14,7 @@ st.markdown("---")
 
 st.header("Apa itu PrimeSign?")
 st.markdown("""
-**PrimeSign** adalah sebuah platform digital berbasis website dan mobile yang dirancang untuk mendukung **komunikasi serta kemandirian penyandang disabilitas**, khususnya tunarungu dan tunawicara. 
+**PrimeSign** adalah sebuah platform digital berbasis website yang dirancang untuk mendukung **komunikasi serta kemandirian penyandang disabilitas**, khususnya tunarungu dan tunawicara. 
 
 Platform ini memanfaatkan teknologi Artificial Intelligence (AI) terkini untuk menerjemahkan **Bahasa Isyarat Indonesia (BISINDO)** melalui tiga metode: **gambar, video, dan real-time** menggunakan kamera. Dengan kemampuan tersebut, PrimeSign menghadirkan solusi komunikasi yang lebih cepat, akurat, dan mudah diakses oleh masyarakat luas.
 """)
